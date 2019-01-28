193191289f6a88ec5cf1ace7e6e3c2b79c2c9599
CHuynh97 <chris.huynh17@gmail.com>
2019-01-28 17:53:39 -0500

    Implemented m/m/1/k

--- lab1/main.py
+++ lab1/main.py
@@ -7,23 +7,28 @@ import time
 
 def main(args):
     T, L, C = (
-        1,
+        100,
         2000,
         1000000
     )
+    K = 10
     E_N = []
     P_idle = []
-    rhos = np.arange(0.25, 0.65, 0.1)
+    rhos = np.arange(1.5, 1.6, 0.1)
     for i, rho in enumerate(rhos):
         t0 = time.time()
         event_queue = simulator.generate_events(T, L, C, rho)
-        observer_records = simulator.DES_Simulator(event_queue)
-        E_N.append(observer_records[-1].E_N)
-        P_idle.append(observer_records[-1].P_idle)
-        t1 = time.time()
-        print("Finished Test {}: rho={}, elapsed time: {}s".format(i, rho, t1-t0))
-    plt.plot(rhos, E_N)
-    plt.show()
+        observer_records = simulator.DES_Simulator(event_queue, L, C, K)
+        print(observer_records[-1])
+    #     E_N.append(observer_records[-1].E_N)
+    #     P_idle.append(observer_records[-1].P_idle)
+    #     t1 = time.time()
+    #     print("Finished Test {}: rho={}, elapsed time: {}s".format(i, rho, t1-t0))
+    # plt.plot(rhos, P_idle)
+    # plt.xlabel("rho")
+    # plt.ylabel("P_idle")
+    # plt.title("Question 3.2: P_idle vs rho")
+    # plt.show()
 
 if __name__ == "__main__":
     main(sys.argv)
--- lab1/simulator.py
+++ lab1/simulator.py
@@ -28,74 +28,92 @@ class ObserverRecord:
         E_N: The time-average number of packets in the buffer E[N].
         P_idle: The proportion of time the server is idle.
     """
-    def __init__(self, E_N, P_idle):
+    def __init__(self, E_N, P_idle, P_loss):
         self.E_N = E_N
         self.P_idle = P_idle
+        self.P_loss = P_loss
     
     def __repr__(self):
         return """
 {{
     E_N: {},
-    P_idle: {}
+    P_idle: {},
+    P_loss: {}
 }}
-""".format(self.E_N, self.P_idle)
+""".format(self.E_N, self.P_idle, self.P_loss)
 
 def generate_events(T, L, C, rho):
     """
     Generate Events for DES
     params:
-        T: Time period for simluation
-        L: Average package length in bits
-        C: Service rate
+        T: Time period for simluation (s)
+        L: Average package length (bits)
+        C: Service rate (bps)
         rho: Traffic intensity value
     """
     lam = rho*C/L
     arrivals = [rv.exponential(1/lam)]
-    l = rv.exponential(L)
-    departures = [arrivals[0] + l/C]
     while arrivals[-1] < T:
         a = arrivals[-1] + rv.exponential(1/lam)
-        l = rv.exponential(L)
-        s = l/C
         arrivals.append(a)
-        if a < departures[-1]:
-            departures.append(departures[-1]+s)
-        else:
-            departures.append(a+s)
     observers = [rv.exponential(1/(lam*5))]
     while observers[-1] < T:
-        observers.append(observers[-1]+rv.exponential(1/(lam*5)))
+        o = observers[-1]+rv.exponential(1/(lam*5))
+        observers.append(o)
     arrivals = [DESEvent(e_type="arrival", e_time=a) for a in arrivals[:-1]]
-    departures = [DESEvent(e_type="departure", e_time=d) for d in departures[:-1]]
     observers = [DESEvent(e_type="observer", e_time=o) for o in observers[:-1]]
-    events = arrivals+departures+observers
+    events = arrivals+observers
     return sorted(events, key=lambda x: x.time)
 
-def DES_Simulator(events): #m/m/1
+def DES_Simulator(events, L, C, K): #M/M/1/K
     """
     Discrete Event Simulator
     Iterate through events are calculate stats
     params:
         events: Sorted list of DESEvent objects to iterate over.
+        L: Average package length (bits)
+        C: Output link rate (bps)
+        K: Max buffer size
     """
+    buffer = []
     observer_records = []
     Na, Nd, No = (0, 0, 0)
     idle_count = 0
     avg_queuesize_sum = 0
-    for e in events:
+    num_packet_loss = 0
+    while events or buffer:
+        # Pop event that occurs first
+        if buffer:
+            if events:
+                e = buffer.pop(0) if buffer[0].time < events[0].time else events.pop(0)
+            else:
+                e = buffer.pop(0)
+        else:
+            e = events.pop(0)
         # Conditional block to update Na and Nd
         if e.type == "arrival":
             Na += 1
+            package_length = rv.exponential(L)
+            service_time = package_length/C
+            if len(buffer) < K:    
+                if buffer:
+                    next_departure = buffer[-1]
+                    dept_time = next_departure.time+service_time
+                else:
+                    dept_time = e.time+service_time
+                buffer.append(DESEvent(e_type="departure", e_time=dept_time))
+            else:
+                num_packet_loss += 1
         elif e.type == "departure":
             Nd += 1
         # If observer event record data and add to list
         if e.type == "observer":
             No += 1
-            queue_size = (Na - Nd)
+            queue_size = len(buffer)
             idle_count += 1 if not queue_size else 0
             avg_queuesize_sum += queue_size
+            P_loss=num_packet_loss/Na if Na else 0
             observer_records.append(
-                ObserverRecord(E_N=avg_queuesize_sum/No, P_idle=idle_count/No)
+                ObserverRecord(E_N=avg_queuesize_sum/No, P_idle=idle_count/No, P_loss=P_loss)
             )
-    print("Na={}, Nd={}, No={}".format(Na, Nd, No))
     return observer_records
