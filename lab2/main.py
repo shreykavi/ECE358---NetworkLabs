84402c55aecedec9a776341903ba5b4db4cbd40c
CHuynh97 <chris.huynh17@gmail.com>
2019-03-14 18:04:10 -0400

    added persistent graphs

--- lab2/main.py
+++ lab2/main.py
@@ -1,4 +1,5 @@
 from __future__ import division
+import sys
 import numpy as np
 import math
 from random import randint
@@ -27,14 +28,14 @@ def CSMACD_simulation(persistent, N, A, R, L, D, S, Kmax, T):
 
     def update_queue(q, t):
         i = 0
-        q_length = i < len(q)
-        while q[i] < t and i < q_length:
+        q_length = len(q)
+        while i < q_length and q[i] < t:
             q[i] = t
             i += 1
 
     def get_collision_time(node1, node2, t1, t2):
         distance = abs(node1-node2)*D
-        collision_time = (distance - (t1-t2)*S)/2/S
+        collision_time = (distance - (t2-t1)*S)/2/S
         return t2 + collision_time
     
     transmit_counter = 0
@@ -48,6 +49,7 @@ def CSMACD_simulation(persistent, N, A, R, L, D, S, Kmax, T):
             queues[i].append(queues[i][-1] + exponential(1/A))
     # print([len(queues[i]) for i in queues])
     collision_counts = {i: 0 for i in range(N)}
+    busy_counts = {i: 0 for i in range(N)}
     # Simulate
     while queues:
         first_node = min(queues, key=lambda x: queues[x][0])
@@ -77,10 +79,18 @@ def CSMACD_simulation(persistent, N, A, R, L, D, S, Kmax, T):
         # Condition block for busy
         else:
             # print("SUCCESSFUL TRANSMISSION")
-            busy_packets = [i for i in next_packets if propagation_times[i] < next_packets < propagation_times[i] + transmission_time]
+            busy_packets = [i for i in next_packets if propagation_times[i] < next_packets[i] < propagation_times[i] + transmission_time]
             for i in busy_packets:
-                t_wait = propagation_times[i] + transmission_time if persistent else 0 # make exponential backoff for non persistent
-                new_time = first_arrival + t_wait
+                if persistent:
+                    busy_counts[i] += 1
+                    if busy_counts[i] >= Kmax:
+                        busy_counts[i] = 0
+                        queues[i].pop(0)
+                        if not queues[i]:
+                            del queues[i]
+                        continue                        
+                t_wait = propagation_times[i] + transmission_time if persistent else exponential_backoff(busy_counts[i]) # make exponential backoff for non persistent
+                new_time = t_wait
                 update_queue(queues[i], new_time)
             successful_transmit_counter += 1
             collision_counts[first_node] = 0
@@ -94,24 +104,37 @@ def CSMACD_simulation(persistent, N, A, R, L, D, S, Kmax, T):
     }
 
 def main():
+    persistent = bool(int(sys.argv[1]))
+    if persistent:
+        title = "CSMA/CD Persistent Throughput"
+        print("Simulating Persistent CSMA/CD")
+    else:
+        title = "CSMA/CD Non-Persistent Throughput"
+        print("Simulating Non-Persistent CSMA/CD")
+
     # Parameters for simulation
-    efficiency = []
-    for N in range(20, 120, 20):    
-        parameters = dict(
-            persistent = True,
-            T = 1000,
-            N = N,
-            A = 5,
-            R = 1000000,
-            L = 1500,
-            D = 10,
-            S =  200000000,
-            Kmax = 10
-        )
-        res = CSMACD_simulation(**parameters)
-        efficiency.append(res["successful"]/res["total"])
-        print("Finished test for A={}, N={}".format(parameters['A'], parameters['N']))
-    plt.plot(range(20, 120, 20), efficiency, label="A=5")
+    for A in [7, 10, 20]:
+        throughput = []
+        for N in range(20, 120, 20):    
+            parameters = dict(
+                persistent = persistent,
+                T = 1000,
+                N = N,
+                A = A,
+                R = 1000000,
+                L = 1500,
+                D = 10,
+                S =  200000000,
+                Kmax = 10
+            )
+            res = CSMACD_simulation(**parameters)
+            throughput.append(res["successful"]*parameters['L']/1000000/parameters['T'])
+            print("Finished test for A={}, N={}".format(parameters['A'], parameters['N']))
+        plt.plot(range(20, 120, 20), throughput, label="A={}".format(A))
+    plt.title(title)
+    plt.xlabel("Number of Nodes")
+    plt.ylabel("Efficiency")
+    plt.legend(loc="best")
     plt.show()
 
 if __name__ == "__main__":
